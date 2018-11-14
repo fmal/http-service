8e031f996f4fb95c98e8bc31e8f5aad0ee9906e2
Filip Malinowski <crimhead@gmail.com>
2018-11-14 22:38:13 +0100

    fix(types): fix missing import

--- src/factories/index.d.ts
+++ src/factories/index.d.ts
@@ -1,18 +1,29 @@
-import { Tag } from 'cerebral/tags';
-import { Action } from 'cerebral';
+import { Tag } from 'cerebral';
 
-export function httpAbort(regexp: string | Tag): Action;
+export function httpAbort(regexp: string | Tag): Function;
 export function httpDelete(
   url: string | Tag,
   query?: any,
   options?: any
-): Action;
-export function httpGet(url: string | Tag, query?: any, options?: any): Action;
-export function httpPatch(url: string | Tag, body?: any, options?: any): Action;
-export function httpPost(url: string | Tag, body?: any, options?: any): Action;
-export function httpPut(url: string | Tag, body?: any, options?: any): Action;
+): Function;
+export function httpGet(
+  url: string | Tag,
+  query?: any,
+  options?: any
+): Function;
+export function httpPatch(
+  url: string | Tag,
+  body?: any,
+  options?: any
+): Function;
+export function httpPost(
+  url: string | Tag,
+  body?: any,
+  options?: any
+): Function;
+export function httpPut(url: string | Tag, body?: any, options?: any): Function;
 export function httpUploadFile(
   urlValue: string | Tag,
   filesValue: string[],
   optionsValue: any[]
-): Action;
+): Function;
